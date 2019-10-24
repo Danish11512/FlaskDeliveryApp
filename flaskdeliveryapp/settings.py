6a1a2bd5b20493845f421efb91c43f4a013e16c4
Danish Faruqi <dfaruqi@records.nyc.gov>
2019-10-24 13:26:12 -0400

    repository initialized with basic files

---
+++ flaskdeliveryapp/settings.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Application configuration.
+
+Most configuration is set via environment variables.
+
+For local development, use a .env file to set
+environment variables.
+"""
+import os
+from environs import Env
+
+env = Env()
+env.read_env()
+
+ENV = env.str("FLASK_ENV", default="production")
+DEBUG = ENV == "development"
+SQLALCHEMY_DATABASE_URI = (os.environ.get('DEV_DATABASE_URL') or
+                               'postgresql://developer@127.0.0.1:5432/flaskdeliveryapp')
+SECRET_KEY = env.str("SECRET_KEY")
+BCRYPT_LOG_ROUNDS = env.int("BCRYPT_LOG_ROUNDS", default=13)
+DEBUG_TB_ENABLED = DEBUG
+DEBUG_TB_INTERCEPT_REDIRECTS = False
+CACHE_TYPE = "simple"  # Can be "memcached", "redis", etc.
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+WEBPACK_MANIFEST_PATH = "webpack/manifest.json"
