aa5509d46c4a3a45b65c6192e8731216310ca71e
Danish Faruqi <dfaruqi@records.nyc.gov>
2019-10-25 13:11:43 -0400

    db changes made

---
+++ config.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+dotenv_path = os.path.join(basedir, '.env')
+load_dotenv(dotenv_path)
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    USE_LOCAL_AUTH = os.environ.get('USE_LOCAL_AUTH') == "True"
+
+    USE_LDAP = os.environ.get('USE_LDAP') == "True"
+    LDAP_SERVER = os.environ.get('LDAP_SERVER') or None
+    LDAP_PORT = os.environ.get('LDAP_PORT') or None
+    LDAP_USE_TLS = os.environ.get('LDAP_USE_TLS') == "True"
+    LDAP_KEY_PATH = os.environ.get('LDAP_KEY_PATH') or None
+    LDAP_SA_BIND_DN = os.environ.get('LDAP_SA_BIND_DN') or None
+    LDAP_SA_PASSWORD = os.environ.get('LDAP_SA_PASSWORD') or None
+    LDAP_BASE_DN = os.environ.get('LDAP_BASE_DN') or None
+    LOGIN_REQUIRED = os.environ.get('LOGIN_REQUIRED') == "True"
+    DEFAULT_PASSWORD = os.environ.get('DEFAULT_PASSWORD')
+
+    USE_SAML = os.environ.get('USE_SAML') == "True"
+    SAML_PATH = os.environ.get('SAML_PATH') or None
+
+    USER_DATA = (os.environ.get('USER_DATA') or os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data',
+                                                             'users.csv'))
+    APP_DEV_INTAKE_EMAIL_RECIPIENTS = os.environ.get('APP_DEV_INTAKE_EMAIL_RECIPIENTS', '').split(',') or []
+
+    POSTS_PER_PAGE = 10
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or '127.0.0.1'
+    MAIL_PORT = os.environ.get('MAIL_PORT') or '25'
+
+    FILE_UPLOAD_PATH = os.environ.get('FILE_UPLOAD_PATH') or '/vagrant/app/static/documents'
+    VIRUS_SCAN_ENABLED = os.environ.get('VIRUS_SCAN_ENABLED') == "True"
+    MAX_CONTENT_LENGTH =  512 * 1024 * 1024 # 512 MB
+
+    @staticmethod
+    def init_app(app):
+        pass
